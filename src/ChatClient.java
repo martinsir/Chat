17e62e38ccdb7f30fb9969bedd68ee575f979131
ocn06 <olivian@outlook.dk>
2017-02-20 14:20:47 +0100

    Client main -- Login view

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,5 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Martin H on 20-02-2017.
  */
-public class ChatClient {
+public class ChatClient extends Application{
+
+    public static void main(String[] args) {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Views/Login.fxml"));
+        primaryStage.setTitle("Chat chat");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
 }
