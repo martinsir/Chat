cb1ee226aa5aa9d0218f44bc79333677cca945a8
martinsir <double_pun@hotmail.com>
2017-02-20 13:52:28 +0100

    test

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,5 +1,12 @@
 public class ChatServer {
 
+    // JOIN {user_name}, {server_ip}:{server_port}
+    /* (From client to server. The user name is given by the user. User
+      name is max 12 chars long, only letters, digits, ‘-‘ and ‘_’ allowed.) */
+    // J_OK (From server to client)
+    // J_ERR (From server to client. Client not accepted. Duplicate name, please try again with different user name)
+    // DATA {USer_Name}: {free text}
+    // Alive
     public static void main(String[] args) {
 	// write your code here
     }
